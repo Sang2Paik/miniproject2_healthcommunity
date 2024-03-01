60d2fa6fff5ad7f9c2a32bc54860d5852b603f64
youngjoonlee84 <157359502+youngjoonlee84@users.noreply.github.com>
2024-03-01 21:15:43 +0900

    Update WorkoutCaloryVo.java

--- src/main/java/vo/WorkoutCaloryVo.java
+++ src/main/java/vo/WorkoutCaloryVo.java
@@ -1,55 +1,98 @@
-package vo;
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE mapper
+PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN"
+"http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper namespace="workout">
 
-public class WorkoutCaloryVo {
-	double	cal_per_unit;
-	String	workout_name;
-	int     page;
-	int     perPage;
+	<!-- 내 운동 조회 -->
+	<select id="my_workout_list" parameterType="vo.WorkoutVo" resultType="vo.WorkoutVo">
+
+		select * from
+		(
+			select
+				*, 
+				substring(w_regdate,1,10) as regdate from workout_kcal
+		) w
+		where user_idx= #{user_idx} order by regdate desc, w_idx desc 
+	</select>
 	
+	<!-- 날짜별 운동 조회 -->
+	<select id="my_workout_list_date" parameterType="vo.WorkoutVo" resultType="vo.WorkoutVo">
 	
+		select * from
+		(
+			select
+				*, substring(w_regdate,1,10) as regdate from workout_kcal 
+		) w
+		<trim prefix="where" prefixOverrides="or" >
+			<if test = "w_regdate !=null">
+				user_idx=#{ user_idx } and regdate = #{ w_regdate }
+			</if>
+		</trim>
+		order by w_idx desc
+
+	</select>
 	
-	public WorkoutCaloryVo(int page, int perPage) {
-		super();
-		this.page = page;
-		this.perPage = perPage;
-	}
-	public WorkoutCaloryVo(String workout_name, int page, int perPage) {
-		super();
-		this.workout_name = workout_name;
-		this.page = page;
-		this.perPage = perPage;
-	}
-	public WorkoutCaloryVo(double cal_per_unit, String workout_name) {
-		super();
-		this.cal_per_unit = cal_per_unit;
-		this.workout_name = workout_name;
-	}
-	public double getCal_per_unit() {
-		return cal_per_unit;
-	}
-	public void setCal_per_unit(double cal_per_unit) {
-		this.cal_per_unit = cal_per_unit;
-	}
-	public String getWorkout_name() {
-		return workout_name;
-	}
-	public void setWorkout_name(String workout_name) {
-		this.workout_name = workout_name;
-	}
-	public int getPage() {
-		return page;
-	}
-	public void setPage(int page) {
-		this.page = page;
-	}
-	public int getPerPage() {
-		return perPage;
-	}
-	public void setPerPage(int perPage) {
-		this.perPage = perPage;
-	}
+	<!-- 전체 운동 조회, 칼로리 계산 -->
+	<select id="my_workout_list_cal" parameterType="int" resultType="vo.WorkoutVo">
 	
+		select regdate, sum(w_unit_kcal) as total_sum from
+		(
+			select
+				*, 
+				substring(w_regdate,1,10) as regdate from workout_kcal
+		) w
+		where user_idx=#{ user_idx } group by regdate  order by regdate desc
+
+	</select>
 	
+		<!-- 날짜별 운동 조회, 칼로리 계산 -->
+	<select id="my_workout_list_date_cal" parameterType="vo.WorkoutVo" resultType="vo.WorkoutVo">
+	
+		select regdate, sum(w_unit_kcal) as total_sum from
+		(
+			select
+				*, 
+				substring(w_regdate,1,10) as regdate from workout_kcal
+		) w
+		where user_idx=#{ user_idx} and regdate = #{ w_regdate } order by w_regdate desc
+		
+<!--  		<trim prefix="where" prefixOverrides="or" >
+			<if test = "w_regdate !=null">
+				user_idx = #{ user_idx} 
+			</if>
+		</trim>   -->
+	</select>
+	
+	<!-- 오늘 운동 조회, 칼로리 계산 -->
+	<select id="my_workout_list_cal_today" parameterType="int" resultType="double">
+	
+		select ifnull(total_sum,0) from
+		(
+			select regdate, sum(w_unit_kcal) as total_sum from
+			(
+				select
+					*, 
+					substring(w_regdate,1,10) as regdate from workout_kcal
+			) w
+			where user_idx=#{ user_idx} and regdate = Date_format(now(),'%Y-%m-%d') group by regdate
+		) t
+	</select>
 
 	
-}
+	
+	<!-- 내 운동 입력 -->
+	<insert id="workout_insert" parameterType="vo.WorkoutVo">
+		insert into workout_kcal values(null, #{w_name}, #{w_regdate}, #{w_time}, #{w_unit_kcal}, #{user_idx}, #{user_name})
+	</insert>
+	
+	<!-- 내 운동 삭제 -->
+	<delete id="my_workout_delete" parameterType="int">
+		delete from workout_kcal where w_idx = #{ w_idx }
+	</delete>
+
+	<select id="my_workout_select_one" parameterType="int" resultType="vo.WorkoutVo">
+		select * from workout_kcal where w_idx = #{ w_idx } 
+	</select>
+	
+</mapper>
